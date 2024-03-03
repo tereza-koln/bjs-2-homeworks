ca400691a2d73717b40d245f9bb3230cb8dae4b8
Tereza Koln <ruanapoor@gmail.com>
2024-03-03 11:55:20 +0300

    second

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,33 @@
 "use strict"
+
 function solveEquation(a, b, c) {
 
-  let arr = [];
-  let discriminant = b ** 2 - 4 * a * c;
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
 
-  if (discriminant < 0) {
-    let arr = [] }
-  else if (discriminant === 0) {
-    let root = -b / ( 2 * a );
-    let arr =[]
-    arr.push(+root.toFixed(2)); }
-  else if (discriminant > 0) {
-    let arr = [] } {
-    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
-    arr.push((-b - Math.sqrt(discriminant)) / (2 * a)); }
+	 if (discriminant === 0) {
+		let root = -b / (2 * a);
+		let arr = []
+		arr.push(+root.toFixed(2));
+	} else if (discriminant > 0) {
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr = [];
+		arr.push(+root1.toFixed(2), +root2.toFixed(2));
+	}  else {
+    arr = [];
+  }  
   
-  return arr;
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
-  let monthPercent = +percent / 100 / 12;
-  let principal = +amount - +contribution;
-  let monthlyPayment = principal * (monthPercent + (monthPercent / (((1 + monthPercent) ** +countMonths) - 1)));
-  let sum = +(monthlyPayment * countMonths).toFixed(2);
-  
-  return sum;
+	let monthPercent = percent / 100 / 12;
+	let bodyOfCredit = amount - contribution;
+	let monthlyPayment = bodyOfCredit * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
+	let totalAmount = +(monthlyPayment * countMonths).toFixed(2);
+
+	return totalAmount;
+
 }
