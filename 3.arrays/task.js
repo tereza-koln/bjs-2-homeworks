07493677a98e8d3ecce4c8a253c44adcf9fe5683
Tereza K <134049891+tereza-koln@users.noreply.github.com>
2024-03-31 06:41:27 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	return arr1.every((element, index) => element === arr2[index]);
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => {
+		acc += item;
+		if (index === arr.length - 1) {
+			return acc / arr.length;
+		}
+		return acc;
+	}, 0);
+	return result;
 }
