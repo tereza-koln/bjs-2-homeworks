7afe7e1f8a6a3b220bbafd12229170d3052ea92a
Tereza K <134049891+tereza-koln@users.noreply.github.com>
2024-04-04 07:28:54 +0300

    Update task.js

---
+++ 5.classes/task.js
@@ -0,0 +1,123 @@
+//задача 1
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.type = null;
+		this.state = 100;
+	}
+
+	fix() {
+		this.state *= 1.5;
+		if (this.state > 100) {
+			this.state = 100;
+		}
+	}
+
+	set state(newState) {
+		if (newState < 0) {
+			this._state = 0;
+		} else if (newState > 100) {
+			this._state = 100;
+		} else {
+			this._state = newState;
+		}
+	}
+
+	get state() {
+		return this._state;
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "magazine";
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "book";
+		this.author = author;
+	}
+}
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "novel";
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "fantastic";
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "detective";
+	}
+}
+
+//задача 2
+
+class Library {
+	constructor(name, books) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
+
+	findBookBy(type, value) {
+
+		for (let book of this.books) {
+			if ((type === 'name' && value === book.name) || (type === 'author' && value === book.author) || (type === 'releaseDate' && value === book.releaseDate) || (type === 'pagesCount' && value === book.pagesCount)) {
+				return book;
+			}
+		}
+		return null;
+	}
+
+	giveBookByName(bookName) {
+		for (let book of this.books) {
+			if (book.name === bookName) {
+				this.books.splice(this.books.indexOf(book), 1)
+				return book;
+			}
+		}
+		return null;
+	}
+}
+
+const library = new Library("Центральная городская библиотека");
+
+const novel = new DetectivelBook("Эдгар Аллан По", "Падение дома Ашеров", 1839, 68);
+const magazine = new NovelBook("Франц Кафка", "Превращение" 1912, 416);
+library.addBook(detective);
+library.addBook(novel);
+
+const bookOf1919Year = library.findBookBy('releaseDate', 1919);
+if (!bookOf1919Year) {
+	const newBookOf1919Year = new Book("Герман Гессе", "Демиан", 1919, 224);
+	library.addBook(newBookOf1919Year);
+}
+
+
+const givenBook = library.giveBookByName("Превращение");
+givenBook.state = 10;
+
+givenBook.fix();
+
+library.addBook(givenBook);
